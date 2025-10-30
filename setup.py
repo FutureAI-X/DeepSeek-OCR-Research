9b6b8a51b18fc3144ab58485772e5bc09c28db24
FutureAI <futureai@foxmail.com>
2025-10-30 15:08:36 +0800

    DeepSeek OCR 模型仓库中相关文件

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deepseek_ocr_research",
+    version="0.0.1",
+    description="DeepSeek OCR 研究",
+    author="",
+    packages=find_packages()
+
+)
