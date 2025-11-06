18f4c58120c87eb9ae52c3420803db9ce390a638
FutureAI <futureai@foxmail.com>
2025-11-06 15:22:00 +0800

    模型代码改造

--- reproduct/modeling_deepseek_ocr.py
+++ reproduct/modeling_deepseek_ocr.py
@@ -1,6 +1,9 @@
 from transformers import Qwen3Model, Qwen3ForCausalLM
 from transformers.modeling_outputs import BaseModelOutputWithPast, CausalLMOutputWithPast
 from transformers.cache_utils import Cache
+from transformers.processing_utils import Unpack
+from transformers.utils import TransformersKwargs
+
 from configuration_deepseek_ocr import DeepseekOCRConfig
 
 from deepencoder_sam import build_sam_vit_b
@@ -17,7 +20,7 @@ class DeepseekOCRModel(Qwen3Model):
     config_class = DeepseekOCRConfig
 
     def __init__(self, config: DeepseekOCRConfig):
-        super(DeepseekOCRConfig, self).__init__(config)
+        super(DeepseekOCRModel, self).__init__(config)
 
         # 构建 SAM (Segment Anything Model) ViT-B 模型，用于图像分割和特征提取
         self.sam_model = build_sam_vit_b()
@@ -37,139 +40,31 @@ class DeepseekOCRModel(Qwen3Model):
 
     def forward(
         self,
-        input_ids: torch.LongTensor = None,
+        input_ids: Optional[torch.LongTensor] = None,
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
-        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        past_key_values: Optional[Cache] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
         use_cache: Optional[bool] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
         images: Optional[torch.FloatTensor] = None,
         images_seq_mask: Optional[torch.FloatTensor] = None,
         images_spatial_crop: Optional[torch.FloatTensor] = None,
-        return_dict: Optional[bool] = None,
+        **kwargs: Unpack[TransformersKwargs],
     ) -> Union[Tuple, BaseModelOutputWithPast]:
-        if inputs_embeds is None:
-            # inputs_embeds = self.embed_tokens(input_ids)
-            inputs_embeds = self.get_input_embeddings()(input_ids)
         
         sam_model = getattr(self, 'sam_model', None)
-        # sam_model = self.sam_model
         vision_model = getattr(self, 'vision_model', None)
 
-        if sam_model is not None and (input_ids.shape[1] != 1 or self.training) and torch.sum(images[0][1]).item() != 0:
-            idx = 0
-            for image, crop_shape in zip(images, images_spatial_crop):
-                images_in_this_batch = []
-
-                patches = image[0]
-                image_ori = image[1]
-
-                with torch.no_grad():
-                # with torch.inference_mode(): 
-                    
-                    if torch.sum(patches).item() != 0:
-                        # P, C, H, W = patches.shape
-                        crop_flag = 1
-                        local_features_1 = sam_model(patches)
-
-                        local_features_2 = vision_model(patches, local_features_1)  
-                        # vit_time = time.time()
-                        local_features = torch.cat((local_features_2[:, 1:], local_features_1.flatten(2).permute(0, 2, 1)), dim=-1) 
-                        local_features = self.projector(local_features)
-
-
-                        global_features_1 = sam_model(image_ori)
-                        global_features_2 = vision_model(image_ori, global_features_1) 
-                        global_features = torch.cat((global_features_2[:, 1:], global_features_1.flatten(2).permute(0, 2, 1)), dim=-1) 
-                        global_features = self.projector(global_features)
-
-                        print('=====================')
-                        print('BASE: ', global_features.shape)
-                        print('PATCHES: ', local_features.shape)
-                        print('=====================')
-
-                        _, hw, n_dim = global_features.shape
-                        h = w = int(hw ** 0.5)
-
-                        _2, hw2, n_dim2 = local_features.shape
-                        h2 = w2 = int(hw2 ** 0.5)
-
-                        width_crop_num, height_crop_num = crop_shape[0], crop_shape[1]
-
-                        global_features = global_features.view(h, w, n_dim)
-
-                        global_features = torch.cat(
-                            [global_features, self.image_newline[None, None, :].expand(h, 1, n_dim)], dim=1
-                        )
-
-                        global_features = global_features.view(-1, n_dim)
-
-
-                        local_features = local_features.view(height_crop_num, width_crop_num, h2, w2, n_dim2).permute(0, 2, 1, 3, 4).reshape(height_crop_num*h2, width_crop_num*w2, n_dim2)
-                        local_features = torch.cat(
-                            [local_features, self.image_newline[None, None, :].expand(height_crop_num * h2, 1, n_dim2)], dim=1
-                        )
-                        local_features = local_features.view(-1, n_dim2)
-
-                        global_local_features = torch.cat([local_features, global_features, self.view_seperator[None, :]], dim=0)
-
-                        # end_time = time.time()
-
-                        # print('sam: ', sam_time - start_time)
-                        # print('vit: ', vit_time - sam_time)
-                        # print('all: ', end_time - start_time)
-
-                        # exit()
-                   
-                    else:
-                        global_features_1 = sam_model(image_ori)
-                        global_features_2 = vision_model(image_ori, global_features_1) 
-                        global_features = torch.cat((global_features_2[:, 1:], global_features_1.flatten(2).permute(0, 2, 1)), dim=-1) 
-                        global_features = self.projector(global_features)
-                        print('=====================')
-                        print('BASE: ', global_features.shape)
-                        print('NO PATCHES')
-                        print('=====================')
-                        _, hw, n_dim = global_features.shape
-                        h = w = int(hw ** 0.5)
-
-
-                        global_features = global_features.view(h, w, n_dim)
-
-                        global_features = torch.cat(
-                            [global_features, self.image_newline[None, None, :].expand(h, 1, n_dim)], dim=1
-                        )
-
-                        global_features = global_features.view(-1, n_dim)
-
-                        global_local_features = torch.cat([global_features, self.view_seperator[None, :]], dim=0)
-
-                    images_in_this_batch.append(global_local_features)
-                
-
-                # print(inputs_embeds.shape)
-
-                if images_in_this_batch:
-                    images_in_this_batch = torch.cat(images_in_this_batch, dim=0)
-                    # exit()
-
-                    inputs_embeds[idx].masked_scatter_(images_seq_mask[idx].unsqueeze(-1).cuda(), images_in_this_batch)
-
-                idx += 1
-
-
         return super(DeepseekOCRModel, self).forward(
-            input_ids=None, 
-            attention_mask=attention_mask, 
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
             past_key_values=past_key_values,
-            inputs_embeds=inputs_embeds, 
-            use_cache=use_cache, 
-            position_ids = position_ids,
-            output_attentions=output_attentions, 
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            cache_position=cache_position,
+            **kwargs,
         )
     
 class DeepseekOCRForCausalLM(Qwen3ForCausalLM):
@@ -178,83 +73,55 @@ class DeepseekOCRForCausalLM(Qwen3ForCausalLM):
     def __init__(self, config):
         super(Qwen3ForCausalLM, self).__init__(config)
         self.model = DeepseekOCRModel(config)
-
         self.vocab_size = config.vocab_size
-
         self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
-
-        # self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
 
         # Initialize weights and apply final processing
         self.post_init()
 
-    def get_model(self):
-        return self.model
-
 
     def forward(
         self,
-        input_ids: torch.LongTensor = None,
+        input_ids: Optional[torch.LongTensor] = None,
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
-        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        past_key_values: Optional[Cache] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
         labels: Optional[torch.LongTensor] = None,
         use_cache: Optional[bool] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+        logits_to_keep: Union[int, torch.Tensor] = 0,
         images: Optional[torch.FloatTensor] = None,
         images_seq_mask: Optional[torch.FloatTensor] = None,
         images_spatial_crop: Optional[torch.FloatTensor] = None,
-        return_dict: Optional[bool] = None,
-        
-    ) -> Union[Tuple, CausalLMOutputWithPast]:
-        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-        output_hidden_states = (
-            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
-        )
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        **kwargs: Unpack[TransformersKwargs],
 
-        outputs  = self.model(
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+
+        outputs: BaseModelOutputWithPast = self.model(
             input_ids=input_ids,
-            past_key_values=past_key_values,
             attention_mask=attention_mask,
             position_ids=position_ids,
+            past_key_values=past_key_values,
             inputs_embeds=inputs_embeds,
             use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
+            cache_position=cache_position,
             images=images,
-            images_seq_mask = images_seq_mask,
-            images_spatial_crop = images_spatial_crop,
-            return_dict=return_dict
-            
+            images_seq_mask=images_seq_mask,
+            images_spatial_crop=images_spatial_crop,
+            **kwargs,
         )
-        
-        # print(transformer_outputs)
 
-        hidden_states = outputs[0]
-        logits = self.lm_head(hidden_states)
-        logits = logits.float()
+        hidden_states = outputs.last_hidden_state
+        # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
+        slice_indices = slice(-logits_to_keep, None) if isinstance(logits_to_keep, int) else logits_to_keep
+        logits = self.lm_head(hidden_states[:, slice_indices, :])
 
         # logits
 
         loss = None
         if labels is not None:
-            # Shift so that tokens < n predict n
-            shift_logits = logits[..., :-1, :].contiguous()
-            shift_labels = labels[..., 1:].contiguous()
-            # Flatten the tokens
-            loss_fct = CrossEntropyLoss()
-            shift_logits = shift_logits.view(-1, self.config.vocab_size)
-            shift_labels = shift_labels.view(-1)
-            # Enable model parallelism
-            shift_labels = shift_labels.to(shift_logits.device)
-            loss = loss_fct(shift_logits, shift_labels)
-
-        if not return_dict:
-            output = (logits,) + outputs[1:]
-            return (loss,) + output if loss is not None else output
+            loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.vocab_size, **kwargs)
 
         return CausalLMOutputWithPast(
             loss=loss,
@@ -263,78 +130,3 @@ class DeepseekOCRForCausalLM(Qwen3ForCausalLM):
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
-    
-    def prepare_inputs_for_generation(
-        self, input_ids, past_key_values=None, attention_mask=None, inputs_embeds=None, **kwargs
-    ):
-        # Omit tokens covered by past_key_values
-        past_length = 0
-        if past_key_values is not None:
-            if isinstance(past_key_values, Cache):
-                cache_length = past_key_values.get_seq_length()
-                past_length = past_key_values.seen_tokens
-                max_cache_length = past_key_values.get_max_length()
-            else:
-                cache_length = past_length = past_key_values[0][0].shape[2]
-                max_cache_length = None
-
-            # Keep only the unprocessed tokens:
-            # 1 - If the length of the attention_mask exceeds the length of input_ids, then we are in a setting where
-            # some of the inputs are exclusively passed as part of the cache (e.g. when passing input_embeds as
-            # input)
-            if attention_mask is not None and attention_mask.shape[1] > input_ids.shape[1]:
-                input_ids = input_ids[:, -(attention_mask.shape[1] - past_length) :]
-            # 2 - If the past_length is smaller than input_ids', then input_ids holds all input tokens. We can discard
-            # input_ids based on the past_length.
-            elif past_length < input_ids.shape[1]:
-                input_ids = input_ids[:, past_length:]
-            # 3 - Otherwise (past_length >= input_ids.shape[1]), let's assume input_ids only has unprocessed tokens.
-
-            # If we are about to go beyond the maximum cache length, we need to crop the input attention mask.
-            if (
-                max_cache_length is not None
-                and attention_mask is not None
-                and cache_length + input_ids.shape[1] > max_cache_length
-            ):
-                attention_mask = attention_mask[:, -max_cache_length:]
-
-        position_ids = kwargs.get("position_ids", None)
-        if attention_mask is not None and position_ids is None:
-            # create position_ids on the fly for batch generation
-            position_ids = attention_mask.long().cumsum(-1) - 1
-            position_ids.masked_fill_(attention_mask == 0, 1)
-            if past_key_values:
-                position_ids = position_ids[:, -input_ids.shape[1] :]
-
-        # if self.generation_config.cache_implementation == "static":
-        #     # generation with static cache
-        #     cache_position = kwargs.get("cache_position", None)
-        #     if cache_position is None:
-        #         past_length = 0
-        #     else:
-        #         past_length = cache_position[-1] + 1
-        #     input_ids = input_ids[:, past_length:]
-        #     position_ids = position_ids[:, past_length:]
-
-        # TODO @gante we should only keep a `cache_position` in generate, and do +=1.
-        # same goes for position ids. Could also help with continued generation.
-        cache_position = torch.arange(past_length, past_length + position_ids.shape[-1], device=position_ids.device)
-
-        # if `inputs_embeds` are passed, we only want to use them in the 1st generation step
-        if inputs_embeds is not None and past_key_values is None:
-            model_inputs = {"inputs_embeds": inputs_embeds}
-        else:
-            model_inputs = {"input_ids": input_ids}
-
-        model_inputs.update(
-            {
-                "position_ids": position_ids,
-                "past_key_values": past_key_values,
-                "use_cache": kwargs.get("use_cache"),
-                "attention_mask": attention_mask,
-                "images": kwargs.get("images", None),
-                "images_seq_mask": kwargs.get("images_seq_mask", None),
-                "images_spatial_crop": kwargs.get("images_spatial_crop", None),
-            }
-        )
-        return model_inputs
